2e2e943f9a5f3a8b1bea8bde6d6a960653622009
tory <la.destroller@gmail.com>
2024-08-14 16:26:37 -0600

    Se agregaron detalles de villages

--- Objs/Villages/detailsVillages.js
+++ Objs/Villages/detailsVillages.js
@@ -1,15 +1,53 @@
 
-console.log("miau");
 
 const urlS = new URLSearchParams(window.location.search);
 let idMagik = urlS.get('id')
 let contenedorc = document.getElementById("contenedorDetalleVilla")
 let imagend = document.createElement('div')
-imagend.innerHTML =  `
+imagend.innerHTML = `
 <img src="../../Imgs/Villas/${idMagik}.png " class="card-img-top h-50 p-2"> `
 contenedorc.appendChild(imagend)
 
-console.log(idMagik);
+obtenerPersonajes(idMagik)
+
+function obtenerPersonajes(number) {
+    fetch("https://narutodb.xyz/api/village/" + number)
+        .then(response => response.json())
+        .then(data => {
+            pintarPersonajesVillas(data.characters);
+        })
+        .catch(e => console.log(e))
+}
+
+
+function pintarPersonajesVillas(array) {
+    if (array.length > 0) {
+        let contenedorPerson = document.getElementById("charactersVillas")
+        for (let i = 0; i < array.length; i++) {
+
+            fetch("https://narutodb.xyz/api/character/" + array[i].id)
+                .then(response => response.json())
+                .then(data => {
+                    let tarjeta = document.createElement('div')
+                    tarjeta.className = "card col-md-4"
+                    tarjeta.innerHTML = `
+// <img src="${data.images[0]} " class="card-img-top h-50 p-2">
+
+<div class="card-body text-center d-flex row">
+<h5 class="card-title fw-bold"> ${data.name} </h5>
+</div>`
+                    contenedorPerson.appendChild(tarjeta)
+                })
+        }
+    }
+    else{
+        let contenedorPerson = document.getElementById("charactersVillas")
+        let tarjeta = document.createElement('div')
+                    tarjeta.className = "card col-md-4 fw-bolder text-center"
+                    tarjeta.innerHTML = `Characters Not Found :( `
+                    contenedorPerson.appendChild(tarjeta)
+    }
+}
 
 
 // arregloEventos.forEach(element => {
@@ -35,7 +73,7 @@ console.log(idMagik);
 //         <h5 class="card-title fw-bold"> ${data.name} </h5>
 //         <h5 class="card-title fw-bold"> Characters: ${data.characters.length} </h5>
 //       <div class="d-flex justify-content-between align-self-end">
-//         <a href="./detailsVillages.html?id=${data.id}" class="btn btn-primary ms-auto" id="detailsB">Characters</a>  
+//         <a href="./detailsVillages.html?id=${data.id}" class="btn btn-primary ms-auto" id="detailsB">Characters</a>
 //     </div>
 //         </div> `
 //             contenedorVillages.appendChild(tarjeta)
--- Objs/Villages/villages.js
+++ Objs/Villages/villages.js
@@ -1,6 +1,33 @@
-import { pintarVillas } from "../../Modules/Modules.js"
+import { filtroChecks, pintarVillas, filtroDeTexto, pintarC, villas } from "../../Modules/Modules.js"
 
-document.addEventListener("DOMContentLoaded", () => {
-    pintarVillas()
 
-});
+pintarVillas(villas)
+
+document.getElementById("busqueda").addEventListener('keyup', e => {
+    let filtradosTexto = filtroDeTexto(villas)
+    let checksConPalomita = [...document.querySelectorAll('input[type = checkbox]:checked')];
+    checksConPalomita = checksConPalomita.map(e => e.value)
+
+    if (checksConPalomita == 0) {
+        pintarC(filtradosTexto)
+    }
+    else {
+        let arregloConChecks = filtroChecks(filtradosTexto)
+        pintarC(arregloConChecks)
+    }
+
+})
+
+// Categorías filtradas
+
+document.getElementById("makeCategorie").addEventListener('change', e => {
+    let arregloConChecks = filtroChecks(arregloEventos)
+    if (arregloConChecks.length == 0) {
+        let nuevoFiltro = filtroDeTexto(villas)
+        pintarVillas(nuevoFiltro)
+    } else {
+        let filtradosTexto = filtroDeTexto(arregloConChecks)
+        pintarC(filtradosTexto)
+    }
+})
+
